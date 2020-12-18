a72b1e525064e2dcb4b83b33719834b1b3613bc5
Thomas <thomas@DevPad>
2020-12-18 16:05:53 +0100

    Improved definition of objective

--- knapsack.py
+++ knapsack.py
@@ -1,13 +1,12 @@
-from __future__ import print_function
 from ortools.linear_solver import pywraplp
 
 solver = pywraplp.Solver.CreateSolver("SCIP")
 
 # parameters
-weight = 8
+weight = 10
 names = ["Kochplatte", "Zeitschrift", "Brille"]
-weights = [8, 1, 1]
-values = [7, 9, 12]
+weights = [6, 5, 5]
+values = [11, 6, 6]
 items = list(range(len(names)))
 
 # variables
@@ -16,20 +15,26 @@ for i in items:
     x[i] = solver.BoolVar("x[%i]" % i)
 
 # objective
-objective = solver.Objective()
-for i in items:
-    objective.SetCoefficient(x[i], values[i])
-objective.SetMaximization()
+solver.Maximize(sum(x[i] * values[i] for i in items))
 
 # constraints
 solver.Add(sum(x[i] * weights[i] for i in items) <= weight)
 
-solver.Solve()
-print("Pack folgendes ein:")
-print(
-    "Gewicht: %i/%i" % (sum(x[i].solution_value() * weights[i] for i in items), weight)
-)
-print("Wert: %i" % sum(x[i].solution_value() * values[i] for i in items))
-for i in items:
-    if x[i].solution_value() == 1:
-        print(names[i])
+status = solver.Solve()
+if status == pywraplp.Solver.OPTIMAL:
+    print("Objective value =", solver.Objective().Value())
+    print("Problem solved in %f milliseconds" % solver.wall_time())
+    print("Problem solved in %d iterations" % solver.iterations())
+    print("Problem solved in %d branch-and-bound nodes" % solver.nodes())
+    print()
+    print("Pack folgendes ein:")
+    print(
+        "Gewicht: %i/%i"
+        % (sum(x[i].solution_value() * weights[i] for i in items), weight)
+    )
+    print("Wert: %i" % sum(x[i].solution_value() * values[i] for i in items))
+    for i in items:
+        if x[i].solution_value() == 1:
+            print(names[i])
+else:
+    print("Error: Problem is infeasible.")
